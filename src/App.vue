ca279348f28b8405fb88131d34b2e4f5a9149f0d
Maximilian Herzog <maxi.herzog@t-online.de>
2020-03-28 16:45:55 +0100

    Added playbutton

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,13 @@ export default {
         font-family: 'Rubik', sans-serif;
         line-height: 1.4;
     }
+    .noselect {
+      -webkit-touch-callout: none; /* iOS Safari */
+        -webkit-user-select: none; /* Safari */
+         -khtml-user-select: none; /* Konqueror HTML */
+           -moz-user-select: none; /* Old versions of Firefox */
+            -ms-user-select: none; /* Internet Explorer/Edge */
+                user-select: none; /* Non-prefixed version, currently
+                                      supported by Chrome, Opera and Firefox */
+    }
 </style>
--- src/components/InternalizationPlayer.vue
+++ src/components/InternalizationPlayer.vue
@@ -1,34 +1,69 @@
 <template>
-    <p>Player</p>
+
+  <p class="play-button noselect" v-on:click="clickPlayer"><i class="fas fa-play"></i> Play Sound</p>
+  <!--<p class="play-button noselect" v-on:click="clickSinding"><i class="fas fa-play"></i> Sinding - Rustles of Spring Op-32 No-3</p>-->
+
 </template>
 
 <script>
-    /* global MIDI */
-    export default {
-        name: "InternalizationPlayer",
-        created: function () {
-            MIDI.loadPlugin({
-                soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/",
-                instrument: "acoustic_grand_piano",
-                onprogress: function (state, progress) {
-                    console.log(state, progress);
-                },
-                onsuccess: function () {
-                    var delay = 0; // play one note every quarter second
-                    var note = 66; // the MIDI note
-                    var velocity = 127; // how hard the note hits
-                    // play the note
-                    MIDI.setVolume(0, 127);
-                    MIDI.noteOn(0, note, velocity, delay);
-                    MIDI.noteOff(0, note, delay + 0.75);
-                    MIDI.noteOn(0, note + 7, velocity, delay + 0.75);
-                    MIDI.noteOff(0, note + 7, delay + 2 * 0.75);
-                }
-            });
-        }
+
+  /* global MIDI */
+  export default {
+    name: 'InternalizationPlayer',
+    methods: {
+      clickPlayer: function() {
+        MIDI.loadPlugin({
+          soundfontUrl: 'https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/',
+          instrument: 'acoustic_grand_piano',
+          onprogress: function(state, progress) {
+            console.log(state, progress)
+          },
+          onsuccess: function() {
+            var delay = 0 // play one note every quarter second
+            var note = 66 // the MIDI note
+            var velocity = 127 // how hard the note hits
+            // play the note
+            MIDI.setVolume(0, 127)
+            MIDI.noteOn(0, note, velocity, delay)
+            MIDI.noteOff(0, note, delay + 0.75)
+            MIDI.noteOn(0, note + 7, velocity, delay + 0.75)
+            MIDI.noteOff(0, note + 7, delay + 2 * 0.75)
+          }
+        })
+      }
+      // clickSinding: function() {
+      //     MIDI.loadPlugin({
+      //       soundfontUrl: "https://gleitz.github.io/midi-js-soundfonts/FluidR3_GM/",
+      //       instrument: "acoustic_grand_piano",
+      //       onprogress: function (state, progress) {
+      //           console.log(state, progress);
+      //       },
+      //       onsuccess: function () {
+      //
+      //
+      //       }
+      //     });
+      //     function onsuccess(){
+      //       MIDI.Player.start();
+      //     }
+      //     // Sinding - Rustles of Spring Op-32 No-3
+      //     MIDI.Player.loadFile("../assets/fruehlingsrauschen.mid", onsuccess);
+      //     //MIDI.Player.start();
+      //
+      // }
     }
+  }
+
 </script>
 
 <style scoped>
+
+  .play-button {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+    border-top: 1px #ccc dotted;
+    cursor: pointer;
+  }
 
 </style>
