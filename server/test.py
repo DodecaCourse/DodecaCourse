d71b43e83c80b46d21ff1dfb68f3f34b9847d5b8
Maximilian Herzog <maxi.herzog@t-online.de>
2020-04-02 01:28:17 +0200

    Basic cookie functionality

--- server/test.py
+++ server/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import *
 from flask_cors import CORS
 
 
@@ -14,10 +14,25 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 
 # sanity check route
-@app.route('/ping', methods=['GET'])
+# @app.route('/ping', methods=['POST', 'GET'])
+# def ping_pong():
+#     return jsonify('pong! hehe, test<b>TEST</b>')
+@app.route('/setcookie', methods=['POST', 'GET'])
 def ping_pong():
-    return jsonify('pong! hehe, test')
+    if request.method == 'POST':
+        user = request.form['nm']
+        resp = make_response("test")
+        resp.set_cookie('userID', user)
+        print("#---- Set cookie on userID", user)
+        print(request.url)
+        redirect(request.url)
+        return("cookie['userId']:", user)
+    return("no cookie set")
 
+@app.route('/getcookie', methods=['GET'])
+def getcookie():
+   name = request.cookies.get('userID')
+   return jsonify(name)
 
 if __name__ == '__main__':
     app.run()
--- src/views/backend/ServerTest.vue
+++ src/views/backend/ServerTest.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="article">
+<div class="article">
   <h1>Hello World</h1>
-  <p> {{msg}} </p>
-  </div>
+  <v-form method="post" action="http://localhost:5000/setcookie">
+    <v-text-field name="nm" label="userId" solo></v-text-field>
+    <v-btn type="submit" rounded color="primary" dark>Set Cookie</v-btn>
+  </v-form>
+  <p><b>msg:</b> {{msg}} </p>
+</div>
 </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-  name: 'Ping',
+  name: 'ServerTest',
   data() {
     return {
       msg: '',
     };
   },
   methods: {
-    getMessage() {
-      const path = 'http://localhost:5000/ping';
+    getCookie() {
+      const path = 'http://localhost:5000/getcookie';
       axios.get(path)
         .then((res) => {
           this.msg = res.data;
@@ -29,7 +33,7 @@ export default {
     },
   },
   created() {
-    this.getMessage();
+    this.getCookie();
   },
 };
 </script>
