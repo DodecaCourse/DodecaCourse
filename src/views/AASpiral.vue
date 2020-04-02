ad861703da3d8e7ee65bb26c4f1dc57e1fed810a
Philipp <philipp.tepel@gmx.de>
2020-04-02 13:39:27 +0200

    1. test

---
+++ src/views/AASpiral.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "AASpiral",
+
+        methods: {
+            playDegree : function (key, degree, octave) {
+                const note;
+            }
+        }
+    }
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/AASpiralSite.vue
@@ -0,0 +1,15 @@
+<template>
+    <AASpiral/>
+</template>
+
+<script>
+    import AASpiral from "./AASpiral";
+    export default {
+        name: "_SpiralTestSite",
+        components: {AASpiral}
+    }
+</script>
+
+<style scoped>
+
+</style>
