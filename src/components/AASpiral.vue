626da98cc1deea54110c8903de359805c455ab0e
Philipp <philipp.tepel@gmx.de>
2020-04-06 01:43:27 +0200

    Spiral almost done, mostly visuals missing

--- src/components/AASpiral.vue
+++ src/components/AASpiral.vue
@@ -1,11 +1,57 @@
 <template>
   <v-container>
     <v-row align="start">
-      <v-col align="center" md="sm">
+      <v-col align="center" cols="3">
+        <v-chip
+          ><b>Root: {{ getRoot() }}, Octave: {{ this.octave }}</b></v-chip
+        >
+      </v-col>
+
+      <v-col align="center">
+        <v-select :items="bases" v-model="root" label="Base note" dense />
+      </v-col>
+
+      <v-col>
+        <v-card>
+          <select v-model="rootMod" multiple>
+            <option value="b">b (flat)</option>
+            <option value="">(natural)</option>
+            <option value="#"># (sharp)</option>
+          </select>
+        </v-card>
+      </v-col>
+
+      <v-col align="start">
+        <v-card>
+          Octave:
+          <v-btn
+            color="primary"
+            rounded
+            x-small
+            fab
+            elevation="1"
+            v-on:click="octave++"
+            ><b>+</b></v-btn
+          ><v-btn
+            color="primary"
+            rounded
+            x-small
+            fab
+            elevation="1"
+            v-on:click="octave--"
+            ><b>-</b></v-btn
+          >
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row align="start">
+      <v-col align="center" cols="3">
         <v-chip>
           <b>Mode: {{ this.scale }}</b></v-chip
         >
       </v-col>
+
       <v-col>
         <v-card
           class="d-inline-flex px-1 align-start justify-center"
@@ -13,13 +59,18 @@
           width="100%"
         >
           <div v-for="(mode, i) in this.modes" v-bind:key="i">
-            <v-btn rounded color="primary" v-on:click="changeMode(mode)" dark>{{
-              mode.numeral
-            }}</v-btn>
+            <v-btn
+              rounded
+              :color="scale == mode.name ? 'primary' : 'secondary'"
+              v-on:click="changeMode(mode)"
+              dark
+              >{{ mode.numeral }}</v-btn
+            >
           </div>
         </v-card>
       </v-col>
     </v-row>
+
     <v-row>
       <v-col>
         <v-card
@@ -29,7 +80,7 @@
         >
           <div v-for="i in this.intervals" v-bind:key="i">
             <v-btn
-              color="blue"
+              :color="isEnabled(i.halfsteps) ? 'primary' : 'secondary'"
               :disabled="!isEnabled(i.halfsteps)"
               small
               fab
@@ -45,15 +96,17 @@
 </template>
 
 <script>
+import { Note } from "@tonaljs/tonal";
 export default {
   name: "AASpiral",
 
   data: function() {
     return {
       scale: "Major/Ionian",
-      root: { base: "C", flattened: false, sharpened: false },
+      root: "C",
+      rootMod: "",
 
-      octave: 0,
+      octave: 4,
 
       modes: [
         {
@@ -82,49 +135,19 @@ export default {
       ],
 
       intervals: [
-        { halfsteps: 0, name: "P1" },
-        { halfsteps: 1, name: "b2" },
-        { halfsteps: 2, name: "M2" },
-        { halfsteps: 3, name: "b3" },
-        { halfsteps: 4, name: "M3" },
-        { halfsteps: 5, name: "P4" },
-        { halfsteps: 6, name: "A4" },
-        { halfsteps: 7, name: "P5" },
-        { halfsteps: 8, name: "b6" },
-        { halfsteps: 9, name: "M6" },
-        { halfsteps: 10, name: "b7" },
-        { halfsteps: 11, name: "M7" },
-        { halfsteps: 12, name: "P8" }
-      ],
-
-      notes: [
-        { base: "C", flattened: true, sharpened: false },
-        { base: "C", flattened: false, sharpened: false },
-        { base: "C", flattened: false, sharpened: true },
-
-        { base: "D", flattened: true, sharpened: false },
-        { base: "D", flattened: false, sharpened: false },
-        { base: "D", flattened: false, sharpened: true },
-
-        { base: "E", flattened: true, sharpened: false },
-        { base: "E", flattened: false, sharpened: false },
-        { base: "E", flattened: false, sharpened: true },
-
-        { base: "F", flattened: true, sharpened: false },
-        { base: "F", flattened: false, sharpened: false },
-        { base: "F", flattened: false, sharpened: true },
-
-        { base: "G", flattened: true, sharpened: false },
-        { base: "G", flattened: false, sharpened: false },
-        { base: "G", flattened: false, sharpened: true },
-
-        { base: "A", flattened: true, sharpened: false },
-        { base: "A", flattened: false, sharpened: false },
-        { base: "A", flattened: false, sharpened: true },
-
-        { base: "B", flattened: true, sharpened: false },
-        { base: "B", flattened: false, sharpened: false },
-        { base: "B", flattened: false, sharpened: true }
+        { halfsteps: 0, name: "1P" },
+        { halfsteps: 1, name: "2m" },
+        { halfsteps: 2, name: "2M" },
+        { halfsteps: 3, name: "3m" },
+        { halfsteps: 4, name: "3M" },
+        { halfsteps: 5, name: "4P" },
+        { halfsteps: 6, name: "4A" },
+        { halfsteps: 7, name: "5P" },
+        { halfsteps: 8, name: "6m" },
+        { halfsteps: 9, name: "6M" },
+        { halfsteps: 10, name: "7m" },
+        { halfsteps: 11, name: "7M" },
+        { halfsteps: 12, name: "8P" }
       ],
 
       bases: ["C", "D", "E", "F", "G", "A", "B"],
@@ -152,6 +175,10 @@ export default {
   watch: {},
 
   methods: {
+    getRoot: function() {
+      return this.root + this.rootMod;
+    },
+
     changeMode: function(mode) {
       this.scale = mode.name;
       this.changeEnabled(mode);
@@ -177,28 +204,9 @@ export default {
       return this.enabled[i];
     },
 
-    playDegree: function(scale, root, degree, octave) {
-      const note = degree + 7 * octave;
-      window.alert(root + " " + scale + " " + note);
-    },
-
-    noteBt: function(interval) {
-      this.playDegree(this.scale, this.root, interval, this.octave);
-    },
-
-    addInterval: function(note, interval) {
-      if (interval.halfsteps < 5) {
-        return 0;
-      }
-    },
-
-    getNoteString: function(note) {
-      if (note.flattened) {
-        return note.base + "b";
-      } else if (note.sharpened) {
-        return note.base + "#";
-      }
-      return note.base;
+    noteBt: function(index) {
+      let result = Note.transpose(this.getRoot(), index.name);
+      window.alert(result);
     }
   }
 };
