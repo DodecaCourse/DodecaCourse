e2441196918416d22e7fa046d558327e232e0a74
Hans Olischläger <git@hans.olischlaeger.com>
2020-04-09 00:14:01 +0200

    fixed style and behavior of LocaleChooser

--- src/components/LocaleChooser.vue
+++ src/components/LocaleChooser.vue
@@ -1,27 +1,9 @@
 <template>
-
-    <!--<v-menu offset-y open-on-hover>
-
-        <v-btn slot="activator" color="primary" v-on="{ on }">hey</v-btn>
-        <v-list>
-            <v-list-tile
-                    v-for="(loc,index) in locales"
-                    :key="index"
-                    @click="">
-                <v-list-item-title>
-                    {{loc.name}}
-                </v-list-item-title>
-            </v-list-tile>
-        </v-list>
-    </v-menu>-->
     <div class="text-center">
         <v-menu close-on-content-click offset-y right transition="slide-y-transition">
             <template v-slot:activator="{ on }">
-                <v-btn
-                        style="float: right"
-                        v-on="on"
-                >
-                    <v-icon>mdiTranslate</v-icon>
+                <v-btn icon style="float: right" v-on="on" app>
+                    <v-icon>mdi-translate</v-icon>
                 </v-btn>
             </template>
 
@@ -29,8 +11,9 @@
                 <v-list-item
                         v-for="(loc, index) in locales"
                         :key="index"
+                        @click="changeLocaleTo(loc.id)"
                 >
-                    <v-list-item-title @click="foo">{{ loc.name }}</v-list-item-title>
+                    <v-list-item-title>{{ loc.name }}</v-list-item-title>
                 </v-list-item>
             </v-list>
         </v-menu>
@@ -46,21 +29,13 @@
                     {name: "Deutsch", id: "de"},
                     {name: "English", id: "en"},
                 ],
-                items: [
-                    { title: 'Click Me' },
-                    { title: 'Click Me' },
-                    { title: 'Click Me' },
-                    { title: 'Click Me 2' },
-                ],
                 closeOnContentClick: true,
             }
         },
         methods: {
-            foo: function() {
-                if (this.$i18n.locale === "en") {
-                    this.$i18n.locale = "de";
-                } else {
-                    this.$i18n.locale = "en";
+            changeLocaleTo: function(id) {
+                if (this.$i18n.locale !== id) {
+                    this.$i18n.locale = id;
                 }
             }
         }
