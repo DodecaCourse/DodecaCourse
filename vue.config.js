b592c9ac8ee3df9d3f7a65d059b50c6b6c5a9111
Valentin Pratz <git@valentinpratz.de>
2020-04-13 17:58:07 +0200

    Undid last commit as it brought no improvements

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ],
-  configureWebpack:{
-    optimization: {
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-      }
-    }
-  }
+  ]
 }
