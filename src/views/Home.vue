e6eb04ad1efe4792053759da9c8bac69f8404fd6
Hans Olischläger <git@hans.olischlaeger.com>
2020-04-15 01:42:32 +0200

    Added machine translations to component localization in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,89 +1,36 @@
 <template>
   <v-layout align-center justify-center row>
     <v-flex class="article" xs10>
-      <h1>{{$t('title')}}</h1>
-      <h2>{{$t('1h')}}</h2>
-      <p>{{$t('1p')}}</p>
-      <p>{{$t('2p')}}</p>
-      <p>{{$t('3p')}}</p>
-      <h3>What is relative pitch?</h3>
-      <p>
-        For those new to ear training relative pitch can appear a little counterintuitive.
-      </p>
-      <p>
-        With relative pitch you won’t learn to recognise the notes in music themselves. Instead you’ll
-        learn to recognise the relationships between them (relative pitch - relationships, makes sense
-        right?).
-      </p>
-      <p>
-        If you were to hear a single note played, you wouldn’t be able to recognise it with relative pitch.
-        But if a second note is then played you would know what it is in relation to the first note. This
-        means that if you know what any one note is you can recognise any other note (or group of notes)
-        easily, simply using the first note as a reference point.
-      </p>
-      <h3>Why do we focus on relative pitch?</h3>
-      <p>Relative pitch isn’t the only way that musicians can recognise and play music by ear.</p>
-      <p>The other approach is called perfect (or absolute) pitch.</p>
-      <p>
-        With perfect pitch a musician recognises each note itself. So if a single note is played, you could
-        recognise it straightaway if you had perfect pitch. You wouldn’t need a reference point.
-      </p>
-      <p>This sounds ideal but there’s a problem: you’re not going to develop it.</p>
-      <p>
-        Perfect pitch is a hotly debated topic among musicians and I don’t want to weigh in here,
-        (because I want to get past the debating and into the ear training) but there’s a lot of evidence
-        that supports the idea that perfect pitch can only be developed in childhood.
-      </p>
-      <p>
-        As a result, if you don’t have it already you’re better off forgetting about perfect pitch (read more
-        about relative pitch vs perfect pitch)
-      </p>
-      <p>
-        The good (great even!) news is that relative pitch can be developed by anyone at any age, and
-        when approached in the right way it can be done quickly and painlessly, and the benefits are the
-        same (and some would say even greater) than they are with perfect pitch.
-      </p>
-      <p>
-        So how does one approach it in the right way?
-      </p>
-      <p>
-        With a tonality based approach.
-      </p>
-      <h3>What is a tonality based approach to ear training?</h3>
-      <p>A tonality based approach to ear training is built around a single note, called the tonic.</p>
-      <p>
-        The tonic is the central note in a piece of music. It sounds the most stable and at rest and it’s the
-        note that every other note resolves back to. Just about every piece of music ends on the tonic.
-      </p>
-      <h3>Why do we use a tonality based approach?</h3>
-      <p>
-        The reason a tonality based approach to ear training is so effective is because the tonic is the
-        easiest note to learn to recognise and it’s very stable. This makes it the perfect note to use as our
-        reference point (which we need if we want to play music by ear with relative pitch).
-      </p>
-      <p>
-        This approach is very different from the other common approach to ear training, called an
-        interval based approach. An interval based approach focuses on the difference in pitch between
-        each individual note (called an interval). While it makes sense to focus on these intervals it’s
-        useless in practice. You’ll learn to recognise the intervals in the exercises but it won’t help at all
-        when you’re listening to or playing real music because you have no stable reference point to rely
-        on (read more about tonal ear training vs interval ear training)
-      </p>
-      <h3>Summary</h3>
-      <p>
-        So to summarise:
-      </p>
-      <p>
-        Over the coming weeks and months you’re going to transform your playing using a tonality based
-        approach to ear training. You’ll develop a fast and accurate sense of relative pitch that will allow
-        you to quickly and easily recognise any notes or chords you hear or wish to play.
-      </p>
-      <p>
-        If that all makes sense, it’s time to dive in. Click the link below to continue onto the first module
-        where you’ll learn to recognise the tonic (so you have your reference point for all future ear
-        training).
-      </p>
-      <v-btn style="float: right" color="secondary" class="ma-2" to="/tonic-internalization/overview">Start module 1</v-btn>
+      <h1> {{$t('100h1')}} </h1>
+      <h2> {{$t('200h2')}} </h2>
+      <p> {{$t('300p')}} </p>
+      <p> {{$t('400p')}} </p>
+      <p> {{$t('500p')}} </p>
+      <h3> {{$t('600h3')}} </h3>
+      <p> {{$t('700p')}} </p>
+      <p> {{$t('800p')}} </p>
+      <p> {{$t('900p')}} </p>
+      <h3> {{$t('1000h3')}} </h3>
+      <p> {{$t('1100p')}} </p>
+      <p> {{$t('1200p')}} </p>
+      <p> {{$t('1300p')}} </p>
+      <p> {{$t('1400p')}} </p>
+      <p> {{$t('1500p')}} </p>
+      <p> {{$t('1600p')}} </p>
+      <p> {{$t('1700p')}} </p>
+      <p> {{$t('1800p')}} </p>
+      <p> {{$t('1900p')}} </p>
+      <h3> {{$t('2000h3')}} </h3>
+      <p> {{$t('2100p')}} </p>
+      <p> {{$t('2200p')}} </p>
+      <h3> {{$t('2300h3')}} </h3>
+      <p> {{$t('2400p')}} </p>
+      <p> {{$t('2500p')}} </p>
+      <h3> {{$t('2600h3')}} </h3>
+      <p> {{$t('2700p')}} </p>
+      <p> {{$t('2800p')}} </p>
+      <p> {{$t('2900p')}} </p>
+      <v-btn style="float: right" color="secondary" class="ma-2" to="/tonic-internalization/overview"> {{$t('3000v-btn')}} </v-btn>
     </v-flex>
   </v-layout>
 </template>
@@ -102,18 +49,70 @@
 <i18n>
   {
     "en": {
-  "title": "Functional Ear Training",
-  "1h": "The Skill You’ll Learn And The Approach You’ll Use",
-  "1p": "When you first picked up your instrument I’m sure you wanted to jump right in and start playing your favourite tunes. Then you discovered that it’s impossible to play those tunes until you’ve learned the notes and developed a little bit of technique.",
-  "2p": "Ear training is just the same. You might want to jump straight into it so you can begin the wonderful transformation that awaits you (and I want you to as well!) but there are a couple of things you need to understand first, so getting started is nice and easy (and not frustrating!). Fortunately, it will only take a few minutes to learn everything you need to so you can dive in and we’ll cover it all in this short introduction.",
-  "3p": "You’re about to learn about the specific skill you’ll develop with this course (called relative pitch) and the approach you’ll use to develop it (called tonality based ear training). Even if you’re an old hand when it comes to ear training I recommend you give it a quick read as you’ll learn why using a tonality based approach to develop relative pitch is absolutely essential if you want to experience great results with ear training."
-  },
+      "100h1":"Functional Ear Training",
+      "200h2":"The Skill You’ll Learn And The Approach You’ll Use",
+      "300p":"When you first picked up your instrument I’m sure you wanted to jump right in and start playing your favourite tunes. Then you discovered that it’s impossible to play those tunes until you’ve learned the notes and developed a little bit of technique.",
+      "400p":" Ear training is just the same. You might want to jump straight into it so you can begin the wonderful transformation that awaits you (and I want you to as well!) but there are a couple of things you need to understand first, so getting started is nice and easy (and not frustrating!). Fortunately, it will only take a few minutes to learn everything you need to so you can dive in and we’ll cover it all in this short introduction. ",
+      "500p":" You’re about to learn about the specific skill you’ll develop in this course (called relative pitch) and the approach you’ll use to develop it (called tonality based ear training). Even if you’re an old hand when it comes to ear training I recommend you give it a quick read as you’ll learn why using a tonality based approach to develop relative pitch is absolutely essential if you want to experience great results with ear training. ",
+      "600h3":"What is relative pitch?",
+      "700p":" For those new to ear training relative pitch can appear a little counterintuitive. ",
+      "800p":" With relative pitch you won’t learn to recognise the notes in music themselves. Instead you’ll learn to recognise the relationships between them (relative pitch - relationships, makes sense right?). ",
+      "900p":" If you were to hear a single note played, you wouldn’t be able to recognise it with relative pitch. But if a second note is then played you would know what it is in relation to the first note. This means that if you know what any one note is you can recognise any other note (or group of notes) easily, simply using the first note as a reference point. ",
+      "1000h3":"Why do we focus on relative pitch?",
+      "1100p":"Relative pitch isn’t the only way that musicians can recognise and play music by ear.",
+      "1200p":"The other approach is called perfect (or absolute) pitch.",
+      "1300p":" With perfect pitch a musician recognises each note itself. So if a single note is played, you could recognise it straightaway if you had perfect pitch. You wouldn’t need a reference point. ",
+      "1400p":"This sounds ideal but there’s a problem: you’re not going to develop it.",
+      "1500p":" Perfect pitch is a hotly debated topic among musicians and I don’t want to weigh in here, (because I want to get past the debating and into the ear training) but there’s a lot of evidence that supports the idea that perfect pitch can only be developed in childhood. ",
+      "1600p":" As a result, if you don’t have it already you’re better off forgetting about perfect pitch (read more about relative pitch vs perfect pitch) ",
+      "1700p":" The good (great even!) news is that relative pitch can be developed by anyone at any age, and when approached in the right way it can be done quickly and painlessly, and the benefits are the same (and some would say even greater) than they are with perfect pitch. ",
+      "1800p":" So how does one approach it in the right way? ",
+      "1900p":" With a tonality based approach. ",
+      "2000h3":"What is a tonality based approach to ear training?",
+      "2100p":"A tonality based approach to ear training is built around a single note, called the tonic.",
+      "2200p":" The tonic is the central note in a piece of music. It sounds the most stable and at rest and it’s the note that every other note resolves back to. Just about every piece of music ends on the tonic. ",
+      "2300h3":"Why do we use a tonality based approach?",
+      "2400p":" The reason a tonality based approach to ear training is so effective is because the tonic is the easiest note to learn to recognise and it’s very stable. This makes it the perfect note to use as our reference point (which we need if we want to play music by ear with relative pitch). ",
+      "2500p":" This approach is very different from the other common approach to ear training, called an interval based approach. An interval based approach focuses on the difference in pitch between each individual note (called an interval). While it makes sense to focus on these intervals it’s useless in practice. You’ll learn to recognise the intervals in the exercises but it won’t help at all when you’re listening to or playing real music because you have no stable reference point to rely on (read more about tonal ear training vs interval ear training) ",
+      "2600h3":"Summary",
+      "2700p":" So to summarise: ",
+      "2800p":" Over the coming weeks and months you’re going to transform your playing using a tonality based approach to ear training. You’ll develop a fast and accurate sense of relative pitch that will allow you to quickly and easily recognise any notes or chords you hear or wish to play. ",
+      "2900p":" If that all makes sense, it’s time to dive in. Click the link below to continue onto the first module where you’ll learn to recognise the tonic (so you have your reference point for all future ear training). ",
+      "3000v-btn":"Start module 1"
+    },
     "de": {
-      "title": "Funktionale Gehörbildung",
-      "1h": "Die Fähigkeit die du erlangen wirst und der Weg dahin",
-      "1p": "Als du dein Instrument zum ersten Mal in die Hand genommen hast, wolltest du sicher sofort anfangen, deine Lieblingsmusik zu spielen. Dann hast du bemerkt, dass es unmöglich ist, diese Melodien zu spielen, bevor du die Noten gelernt und ein wenig Technik entwickelt hast.",
-      "2p": "Das Gehörtraining ist genau so. Vielleicht möchtest du dich direkt hineinstürzen, damit du die wunderbare Transformation beginnen kannst, die dich erwartet (und ist auch genau richtig), aber es gibt ein paar Dinge, die du zuerst verstehen musst, so dass der Einstieg schön einfach ist (und nicht frustrierend!). Glücklicherweise dauert es nur wenige Minuten, um alles zu lernen, was du brauchst, damit du eintauchen kannst, und wir werden in dieser kurzen Einführung auf alles eingehen.",
-      "3p": "Du wirst gleich die Fähigkeit kennenlernen, die du mit diesem Kurs entwickeln wirst (nämlich relatives Gehör) und den Ansatz, den du für diese Entwicklung verwenden wirst (nämlich tonalitätsbasiertes Gehörtraining). Auch wenn du ein alter Hase in Sachen Gehörbildung bist, empfehle ich dir eine kurze Lektüre, da du lernen wirst, warum die Verwendung eines tonalen Ansatzes zur Entwicklung des relativen Gehörs absolut notwendig ist, wenn du mit der Gehörbildung großartige Ergebnisse erzielen willst."
+      "100h1": "Funktionale Gehörbildung",
+      "200h2": "Die Fähigkeit die du erlangen wirst und der Weg dahin",
+      "300p": "Als du dein Instrument zum ersten Mal in die Hand genommen hast, wolltest du sicher sofort anfangen, deine Lieblingsmusik zu spielen. Dann hast du bemerkt, dass es unmöglich ist, diese Melodien zu spielen, bevor du die Noten gelernt und ein wenig Technik entwickelt hast.",
+      "400p": "Das Gehörtraining ist genau so. Vielleicht möchtest du dich direkt hineinstürzen, damit du die wunderbare Transformation beginnen kannst, die dich erwartet (und ist auch genau richtig), aber es gibt ein paar Dinge, die du zuerst verstehen musst, so dass der Einstieg schön einfach ist (und nicht frustrierend!). Glücklicherweise dauert es nur wenige Minuten, um alles zu lernen, was du brauchst, damit du eintauchen kannst, und wir werden in dieser kurzen Einführung auf alles eingehen.",
+      "500p": "Du wirst gleich die Fähigkeit kennenlernen, die du mit diesem Kurs entwickeln wirst (nämlich relatives Gehör) und den Ansatz, den du für diese Entwicklung verwenden wirst (nämlich tonalitätsbasiertes Gehörtraining). Auch wenn du ein alter Hase in Sachen Gehörbildung bist, empfehle ich dir eine kurze Lektüre, da du lernen wirst, warum die Verwendung eines tonalen Ansatzes zur Entwicklung des relativen Gehörs absolut notwendig ist, wenn du mit der Gehörbildung großartige Ergebnisse erzielen willst.",
+
+      "_comment": "TODO: go over wording of machine translation below",
+      "600h3": "Was ist die relative Tonhöhe?",
+      "700p": "Für diejenigen, die neu am Gehörtraining teilnehmen, kann die relative Tonhöhe ein wenig unintuitiv erscheinen. ",
+      "800p":" Mit relativer Tonhöhe werden Sie nicht lernen, die Noten in der Musik selbst zu erkennen. Stattdessen werden Sie lernen, die Beziehungen zwischen ihnen zu erkennen (relative Tonhöhe - Beziehungen, macht Sinn, richtig?). ",
+      "900p":" Wenn Sie eine einzelne gespielte Note hören würden, könnten Sie sie nicht mehr mit der relativen Tonhöhe erkennen. Wenn dann aber eine zweite Note gespielt wird, wüssten Sie, was sie im Verhältnis zur ersten Note ist. Das bedeutet, dass Sie, wenn Sie wissen, was eine einzelne Note ist, jede andere Note (oder Gruppe von Noten) leicht erkennen können, indem Sie einfach die erste Note als Bezugspunkt verwenden. ",
+      "1000h3": "Warum konzentrieren wir uns auf die relative Tonhöhe?",
+      "1100p": "Die relative Tonhöhe ist nicht die einzige Möglichkeit für Musiker, Musik nach Gehör zu erkennen und zu spielen.",
+      "1200p": "Der andere Ansatz heißt perfekte (oder absolute) Tonhöhe.",
+      "1300p": "Bei perfekter Tonhöhe erkennt ein Musiker jede Note selbst. Wenn also eine einzelne Note gespielt wird, könnte man sie sofort erkennen, wenn man die perfekte Tonhöhe hat. Sie bräuchten keinen Referenzpunkt. ",
+      "1400p": "Das klingt ideal, aber es gibt ein Problem: Sie werden es nicht entwickeln",
+      "1500p": "Das perfekte Gehör ist unter Musikern ein heiß diskutiertes Thema, und ich möchte mich hier nicht einmischen (weil ich die Debatten hinter mir lassen und in das Gehörtraining einsteigen möchte), aber es gibt viele Belege dafür, dass das perfekte Gehör nur in der Kindheit entwickelt werden kann. ",
+      "1600p": "Wenn Sie es noch nicht haben, ist es besser, wenn Sie die perfekte Tonhöhe vergessen (lesen Sie mehr über relative Tonhöhe vs. perfekte Tonhöhe) ",
+      "1700p": "Die gute (und sogar großartige!) Nachricht ist, dass die relative Tonhöhe von jedem in jedem Alter entwickelt werden kann, und wenn man sich ihr auf die richtige Weise nähert, kann man sie schnell und schmerzlos entwickeln, und die Vorteile sind die gleichen (und manche würden sagen, sogar noch größer) als bei einer perfekten Tonhöhe. ",
+      "1800p": "Wie geht man also auf die richtige Weise vor? ",
+      "1900p":" Mit einem auf Tonalität basierenden Ansatz. ",
+      "2000h3": "Was ist ein auf Tonalität basierender Ansatz für das Gehörtraining?",
+      "2100p": "Ein tonalitätsbasierter Ansatz für das Gehörtraining basiert auf einem einzelnen Ton, der Tonika genannt wird.",
+      "2200p": "Die Tonika ist der zentrale Ton in einem Musikstück. Sie klingt am stabilsten und am ruhigsten, und es ist die Note, zu der sich jede andere Note wieder auflöst. Fast jedes Musikstück endet auf der Tonika. ",
+      "2300h3": "Warum verwenden wir einen auf Tonalität basierenden Ansatz?",
+      "2400p": "Der Grund, warum ein auf Tonalität basierender Ansatz für das Gehörtraining so effektiv ist, liegt darin, dass die Tonika die am leichtesten zu erkennende Note ist und sehr stabil ist. Dadurch ist sie der perfekte Ton, den wir als Bezugspunkt verwenden können (den wir brauchen, wenn wir Musik nach Gehör mit relativer Tonhöhe spielen wollen). ",
+      "2500p": "Dieser Ansatz unterscheidet sich stark von dem anderen gebräuchlichen Ansatz für das Gehörtraining, dem sogenannten intervallbasierten Ansatz. Ein intervallbasierter Ansatz konzentriert sich auf den Unterschied in der Tonhöhe zwischen jeder einzelnen Note (Intervall genannt). Obwohl es sinnvoll ist, sich auf diese Intervalle zu konzentrieren, ist es in der Praxis nutzlos. Sie werden lernen, die Intervalle in den Übungen zu erkennen, aber es wird Ihnen überhaupt nicht helfen, wenn Sie echte Musik hören oder spielen, weil Sie keinen stabilen Bezugspunkt haben, auf den Sie sich verlassen können (lesen Sie mehr über tonales Gehörtraining vs. Intervallohrtraining) ",
+      "2600h3": "Zusammenfassung",
+      "2700p": "Um es zusammenzufassen: ",
+      "2800p": "In den kommenden Wochen und Monaten werden Sie Ihr Spiel durch einen auf Tonalität basierenden Ansatz für das Gehörtraining verändern. Sie werden einen schnellen und genauen Sinn für die relative Tonhöhe entwickeln, der es Ihnen erlaubt, schnell und einfach."
     }
   }
 </i18n>
+
+
