3c572ceb4d5591889df7cce9aa5a82baf707a45e
Valentin Pratz <git@valentinpratz.de>
2020-04-17 16:23:50 +0200

    Forgot file RegisterSnack

---
+++ src/components/RegisterSnack.vue
@@ -0,0 +1,132 @@
+<template>
+    <div>
+        <v-container fluid class="cards">
+            <v-snackbar
+                    v-model="showSelf"
+                    vertical
+                    :timeout="0"
+                    color="info"
+            >
+                <v-card>
+                    <template v-if="user == null">
+                        <v-card-text>
+                            <h3 class="mb-1">Generate your personal username</h3>
+                            to save your progress and to continue right where you left off.
+                            By clicking »Generate« below, you allow us to set a cookie to store
+                            that you're logged in with this username.
+                            <v-card-actions>
+                                <v-btn v-on:click="generate">
+                                    <v-icon>mdi-dice-3</v-icon>
+                                    Generate
+                                </v-btn>
+                                <v-spacer />
+                                <v-btn
+                                        primary
+                                        class="mx-5"
+                                        @click="onClose"
+                                >
+                                    Close
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card-text>
+                    </template>
+                    <template v-else>
+                        <v-card-text>
+                            <h3 class="mb-1">Success!</h3>
+                            You are now logged in as <b>{{user.user_keyword}}</b>. Write down your username or bookmark your custom URL:
+                            <v-text-field
+                                    readonly
+                                    solo
+                                    dense
+                                    hide-details
+                                    :value="'https://dodeca.wavel.de/home?usr=' + user.user_keyword"
+                            ></v-text-field>
+                            To log in for your next session, use your custom URL or use the login button above
+                            and enter your username.
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer />
+                            <v-btn
+                                    @click="onClose"
+                                    primary
+                                    class="mx-5"
+                            >
+                                I Understood
+                            </v-btn>
+                        </v-card-actions>
+                    </template>
+                </v-card>
+            </v-snackbar>
+        </v-container>
+    </div>
+
+</template>
+
+<script>
+
+    import api from "../api.js"
+
+    export default {
+        name: 'RegisterSnack',
+        mixins: [api],
+        props: {
+            connected: {
+                Type: Boolean,
+            },
+            show: {
+                type: Boolean,
+                required: true
+            },
+            onClose: {
+                type: Function,
+                required: true
+            }
+        },
+        data: () => ({
+            txtfld: '',
+            login_enabled: false,
+        }),
+        computed: {
+            showSelf: {
+                get: function () {
+                    return this.show;
+                },
+                set: function () {
+                    // updates snackbar state -> ignore
+                }
+            },
+        },
+        methods: {
+            generate: function () {
+                this.generateUser()
+                    .then(u => {
+                        //console.log(u);
+                        this.setCurrentUser(u)
+                            .then(res => {
+                                console.log("Result:", res);
+                            });
+                    });
+            },
+            loginUser: function () {
+                this.setCurrentUser(this.txtfld)
+                    .then(res => {
+                        console.log("Result", res);
+                    });
+            },
+            updateEnableLogin: function () {
+                console.log(this.txtfld.length);
+                if (this.txtfld.length === 4) {
+                    this.getUserID(this.txtfld.trim())
+                        .then(data => {
+                            this.founduser = data;
+                            console.log(data);
+                            this.login_enabled = !(this.founduser == null);
+                        });
+                } else {
+                    this.login_enabled = false;
+                }
+            },
+        },
+    }
+
+</script>
