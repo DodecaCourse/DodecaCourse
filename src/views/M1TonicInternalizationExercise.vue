83afb27b16c57686e3db6c6434435289e565bf28
Hans Olischläger <git@hans.olischlaeger.com>
2020-04-20 19:21:57 +0200

    Detach messages from html and added machine translation to a TonicInternalizationExercise.vue

--- src/views/M1TonicInternalizationExercise.vue
+++ src/views/M1TonicInternalizationExercise.vue
@@ -9,27 +9,17 @@
       xs10
     >
       <h1 class="hidden-xs-only">
-        1. Recognising the Tonic
+        {{ $t("m1.intern-ex.100h1") }}
       </h1>
-      <h2>Tonic Internalisation Exercise</h2>
+      <h2> {{ $t("m1.intern-ex.200h2") }} </h2>
+      <p> {{ $t("m1.intern-ex.300p") }} </p>
+      <p> {{ $t("m1.intern-ex.400p") }} </p>
       <p>
-        The tonic internalisation exercise makes it as easy as possible for
-        you to 'internalise' (memorise) the sound of the tonic.
-      </p>
-      <p>
-        It does this by
-        clearly playing the tonic repeatedly. With this repeated exposure you
-        will quickly start to remember the sound of the tonic.
-      </p>
-      <p>
-        The exercise is
-        made up of a 10 bar sequence, which is repeated throughout the
-        exercise. Hit »Practice« below to listen to this 10 bar sequence.
-        (If you wonder about the icon, you can already take a quick look at
-        the <router-link to="/individual-notes/scale-degrees">
-          scale degrees
+        {{ $t("m1.intern-ex.500p.501") }}
+        <router-link to="/individual-notes/scale-degrees">
+          {{ $t("m1.intern-ex.500p.600router-link") }}
         </router-link>
-        chapter in Module 2)
+        {{ $t("m1.intern-ex.500p.700") }}
       </p>
       <p>
         <InlineConfigurator
@@ -38,14 +28,10 @@
           :config="{degree: 0}"
           hide-test
         >
-          Tonic Internalisation
+          {{ $t("m1.intern-ex.800p.900ic") }}
         </InlineConfigurator>
       </p>
-      <p>
-        The diagram below
-        shows this 10 bar sequence, taken from the tonic internalisation
-        exercise.
-      </p>
+      <p> {{ $t("m1.intern-ex.1000p") }} </p>
       <v-img
         contain
         :class="{'invert-img': $vuetify.theme.dark}"
@@ -54,43 +40,19 @@
         src="/img/m1_c_major_tonic_intern_10bars.svg"
         title="10 Bar Sequence from Tonic Internalisation Exercise"
       />
-      <p>
-        The
-        sequence is split up into two sections, as follows:
-      </p>
+      <p> {{ $t("m1.intern-ex.1200p") }} </p>
       <h4>
-        <b>Bars
-          1-2: Establish one note as the tonic</b>
+        <b> {{ $t("m1.intern-ex.1300h4.1400b") }} </b>
       </h4>
-      <p>
-        In
-        the first two bars, a short chord progression is played (called a I -
-        IV - V progression). This chord progression clearly establishes a
-        single note as the tonic. It's impossible to internalise the sound of
-        the tonic if it isn't clearly present, so you'll become very familiar
-        with it as you progress through the modules as it's used at
-        the beginning of a lot of exercises!
-      </p>
+      <p> {{ $t("m1.intern-ex.1500p") }} </p>
       <h4>
-        <b>Bars
-          3-10: The tonic is played repeatedly</b>
+        <b> {{ $t("m1.intern-ex.1600h4.1700b") }} </b>
       </h4>
-      <p>
-        In
-        the 8 bars that follow, the tonic is played in each bar and the tonic
-        chord is played in every second bar.
-      </p>
+      <p> {{ $t("m1.intern-ex.1800p") }} </p>
       <h4>
-        <b>The
-          Sequence Repeats</b>
+        <b> {{ $t("m1.intern-ex.1900h4.2000b") }} </b>
       </h4>
-      <p>
-        After
-        the 10 bar sequence, the entire sequence is repeated in a new randomly chosen key.
-        The diagram below shows the first 30 bars of the tonic
-        internalisation exercise, and in it you can see that in bar 11, a new
-        key is outlined, this time A major.
-      </p>
+      <p> {{ $t("m1.intern-ex.2100p") }} </p>
       <v-img
         contain
         :class="{'invert-img': $vuetify.theme.dark}"
@@ -99,119 +61,85 @@
         src="/img/m1_tonic_intern_30bars.svg"
         title="First 30 Bars of Tonic Internalisation Exercise"
       />
-      <p>
-        It can be helpful to already start the player and listen to the exercise while you
-        read the instructions. The exercise is repetitive (by design!) so
-        take your time to listen to it and read the instructions to make sure
-        you have a clear understanding of how to use the exercise.
-      </p>
+      <p> {{ $t("m1.intern-ex.2300p") }} </p>
       <ol>
         <li>
           <p>
-            <b>Listen to the
-              <i>I - IV - V</i> Progression:</b> Simply listening to the <i>I - IV - V</i>
-            progression will make it clear to your ear which note is the tonic
-            (even if you can't identify which note it is yet). So just sit back
-            and listen.
+            <b> {{ $t("m1.intern-ex.2400ol.2500li.2600p.2700b.2701") }}
+              <i> {{ $t("m1.intern-ex.2400ol.2500li.2600p.2700b.2800i") }} </i>
+              {{ $t("m1.intern-ex.2400ol.2500li.2600p.2700b.2900") }} </b>
+            {{ $t("m1.intern-ex.2400ol.2500li.2600p.2901") }}
+            <i> {{ $t("m1.intern-ex.2400ol.2500li.2600p.3000i") }} </i>
+            {{ $t("m1.intern-ex.2400ol.2500li.2600p.3100") }}
           </p>
         </li>
         <li>
           <p>
-            <b>Focus your
-              attention on the tonic:</b> for the following 8 bars, listen closely to
-            the tonic that is played as the highest note. Listen to it's stable
-            sound.
+            <b> {{ $t("m1.intern-ex.2400ol.3200li.3300p.3400b") }} </b>
+            {{ $t("m1.intern-ex.2400ol.3200li.3300p.3500") }}
           </p>
         </li>
         <li>
           <p>
-            <b>Sing the
-              tonic:</b> alongside listening to the sound of the tonic, singing it
-            will help you to really hear and internalise it's stable sound.
+            <b> {{ $t("m1.intern-ex.2400ol.3600li.3700p.3800b") }} </b>
+            {{ $t("m1.intern-ex.2400ol.3600li.3700p.3900") }}
           </p>
         </li>
         <li>
           <p>
-            <b>Repeat:</b> when
-            the <i>I - IV - V</i> progression is played in a new key, listen to the
-            progression as you did the first time. This will establish a new
-            note as the tonic and then you can repeat listening to/singing the
-            new tonic.
+            <b> {{ $t("m1.intern-ex.2400ol.4000li.4100p.4200b") }} </b>
+            {{ $t("m1.intern-ex.2400ol.4000li.4100p.4201") }}
+            <i> {{ $t("m1.intern-ex.2400ol.4000li.4100p.4300i") }} </i>
+            {{ $t("m1.intern-ex.2400ol.4000li.4100p.4400") }}
+          </p>
+        </li>
+      </ol>
+      <p> {{ $t("m1.intern-ex.4500p") }} </p>
+      <p> {{ $t("m1.intern-ex.4600p") }} </p>
+      <p> {{ $t("m1.intern-ex.4700p") }} </p>
+      <h3>
+        <b> {{ $t("m1.intern-ex.4800h3.4900b") }} </b>
+      </h3>
+      <ol>
+        <li>
+          <p>
+            <b> {{ $t("m1.intern-ex.5000ol.5100li.5200p.5300b") }} </b>
+            {{ $t("m1.intern-ex.5000ol.5100li.5200p.5400") }}
+          </p>
+        </li>
+        <li>
+          <p>
+            <b> {{ $t("m1.intern-ex.5000ol.5500li.5600p.5700b.5701") }}
+              <i> {{ $t("m1.intern-ex.5000ol.5500li.5600p.5700b.5800i") }} </i>
+              {{ $t("m1.intern-ex.5000ol.5500li.5600p.5700b.5900") }} </b>
+            {{ $t("m1.intern-ex.5000ol.5500li.5600p.6000") }}
           </p>
         </li>
       </ol>
       <p>
-        Practice with this
-        exercise until you start to feel confident that you are internalising
-        the sound of the tonic, or for 3 days (whichever comes first). Then
-        try the tonic internalisation test.
+        <b> {{ $t("m1.intern-ex.6100p.6200b") }} </b>
       </p>
-      <p>
-        That's it! It
-        sounds deceptively simple but you'll be surprised at how effective
-        this technique is.
-      </p>
-      <p>
-        And
-        because of it's deceptive simplicity, many musicians try to
-        complicate things. So here are a few things to avoid:
-      </p>
-      <h3><b>What not to do with the tonic internalisation exercise</b></h3>
-      <ol>
-        <li>
-          <p>
-            <b>DO NOT keep track of the notes/keys:</b> the aim of this exercise is to
-            be able to hear a piece of music and identify (and sing) which note
-            is the tonic. But we aren't trying to say whether that note is a C
-            or an F# (that would be the realm of perfect pitch). Simply listen
-            and sing, with no regard for what the actual notes/keys are.
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>DO NOT try to
-              work out the tonic during the <i>I - IV - V</i> progression:</b> it takes a few
-            moments for our ears to clearly identify a new tonic. We use the I -
-            IV - V progression to make it clear, so wait until after the
-            progression to start actively listening for the tonic. Trying to do
-            it earlier simply makes things more confusing and challenging, with
-            no extra benefit.
-          </p>
-        </li>
-      </ol>
-      <p><b>Finished?</b></p>
       <v-btn
         style="float: right"
         color="secondary"
         class="ma-2"
         to="/tonic-internalization/test"
       >
-        Try the test
+        {{ $t("m1.intern-ex.6300v-btn") }}
       </v-btn>
-      <p>
-        <br>
-      </p><h4>
-        <b>TROUBLESHOOTING</b>
+      <p><br></p>
+      <h4>
+        <b> {{ $t("m1.intern-ex.6400p.6600h4.6700b") }} </b>
       </h4>
-      <b>“I’m struggling to sing in tune”</b>
+      <b> {{ $t("m1.intern-ex.6400p.6800b") }} </b>
+      <p> {{ $t("m1.intern-ex.6400p.6900p") }} </p>
+      <p> {{ $t("m1.intern-ex.6400p.7000p") }} </p>
+      <p> {{ $t("m1.intern-ex.6400p.7100p") }} </p>
       <p>
-        If you’re
-        finding it difficult to sing in tune you may be worried that the described
-        method won’t work for you.
+        {{ $t("m1.intern-ex.6400p.7200p.7201") }}
+        <a href="https://www.successfulsinging.com/singing-guides/singing-in-tune/"> {{ $t("m1.intern-ex.6400p.7200p.7300a") }} </a>
       </p>
-      <p>
-        Fortunately, we
-        have you covered!
-      </p>
-      <p>
-        While many people
-        believe that you can’t fix bad pitch, we know that you can and
-        we’ll prove it to you:
-      </p>
-      <p>
-        Just go and check out <a href="https://www.successfulsinging.com/singing-guides/singing-in-tune/">
-          this introduction to singing in tune</a>.
-      </p>
+
       <!--
             <p>So navigate to the
                 bonus module “How to Sing in Tune” where you’ll find a simple
@@ -231,21 +159,12 @@
                 the correct note in the internalisation exercise and when you’re
                 singing a different note from the chord.</p>
                 -->
+
       <p>
-        <b>“Sometimes the
-          tonic is too high/low for my voice”</b>
+        <b> {{ $t("m1.intern-ex.6400p.7500p.7600b") }} </b>
       </p>
-      <p>
-        If the tonic is
-        played outside your range you can sing it an octave higher or lower
-        than it is sounded.
-      </p>
-      <p>
-        Transposing it by
-        an octave gives you all the benefits of singing it in the octave that
-        you hear it in, with the added benefit of improving your ability to
-        identify high/low passages in music.
-      </p>
+      <p> {{ $t("m1.intern-ex.6400p.7700p") }} </p>
+      <p> {{ $t("m1.intern-ex.6400p.7800p") }} </p>
       <!--
             <p>If you find it
                 difficult to transpose the notes you hear by an octave, you’ll find
